b9361fabc146faa3e650eac2f3ebc082cb909ca2
Javatlacati <scherzo_16@hotmail.com>
2023-12-04 20:07:09 -0600

    Se agrega configuración de enrutamiento para componente de directorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from "./controllers/home/home.component";
+import {DirectoryComponent} from "./controllers/directory/directory.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'search', loadChildren: () => import('./controllers/search/search.module').then(m => m.SearchModule)},
   {path: 'home', component: HomeComponent},
   {path: 'lyrics', loadChildren: () => import('./controllers/lyrics/lyrics.module').then(m => m.LyricsModule)},
+  {path: 'directory', component: DirectoryComponent},
   {path: '**', redirectTo: '/home'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import {FormsModule} from "@angular/forms";
 import {CardModule} from "primeng/card";
 import {MessageService} from "primeng/api";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HomeComponent} from './controllers/home/home.component';
+import { HomeComponent } from './controllers/home/home.component';
 import {TooltipModule} from "primeng/tooltip";
+import { DirectoryComponent } from './controllers/directory/directory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    DirectoryComponent
   ],
   imports: [
     BrowserModule,
