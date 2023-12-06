61604041504540442584712c3b2b5a8e252ceaee
Javatlacati <scherzo_16@hotmail.com>
2023-12-05 20:07:01 -0600

    Carga perezosa de módulo de directorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from "./controllers/home/home.component";
-import {DirectoryComponent} from "./controllers/directory/directory.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'search', loadChildren: () => import('./controllers/search/search.module').then(m => m.SearchModule)},
   {path: 'home', component: HomeComponent},
   {path: 'lyrics', loadChildren: () => import('./controllers/lyrics/lyrics.module').then(m => m.LyricsModule)},
-  {path: 'directory', component: DirectoryComponent},
+  {
+    path: 'directory',
+    loadChildren: () => import('./controllers/directory/directory.module').then(m => m.DirectoryModule)
+  },
   {path: '**', redirectTo: '/home'}
 ];
 
