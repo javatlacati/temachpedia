aecf9ac99cb2303d05fd106fb68bcc493ba1ce3d
Javatlacati <scherzo_16@hotmail.com>
2023-12-10 10:53:18 -0600

    Se agrega comentada la a nueva liga del lambda

--- src/app/controllers/search/services/transcript.service.ts
+++ src/app/controllers/search/services/transcript.service.ts
@@ -13,7 +13,7 @@ export class TranscriptService {
 
   public getTransriptForWord(word: string): Observable<Transcription[]> {
     return this.httpClient.post('http://localhost:8081/api/video-transcription', {
-    // return this.httpClient.post('https://z6hdo7e4zc.execute-api.us-east-1.amazonaws.com/default/temachpedia_search', {
+    // return this.httpClient.post('https://55m31g500e.execute-api.us-east-1.amazonaws.com/default/searchTranscriptionFromPublicElTemachVideos', {
       "title": word
     }) as Observable<Transcription[]>
   }
