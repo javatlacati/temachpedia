f84420b791bce54e7f97601221e716f7ab50db13
Javatlacati <scherzo_16@hotmail.com>
2023-12-13 09:54:10 -0600

    se agregan datos de ejemplo para el componente de salón de trofeos
    Se agrega componente para el salón de trofeos con una prueba de concepto de cambio de fondos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { RouterModule, type Routes } from '@angular/router';
 import { HomeComponent } from './controllers/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {
     path: 'search',
     loadChildren: async () =>
       await import('./controllers/search/search.module').then((m) => m.SearchModule),
   },
-  { path: 'home', component: HomeComponent },
+  {path: 'home', component: HomeComponent},
   {
     path: 'lyrics',
     loadChildren: async () =>
@@ -21,15 +21,20 @@ const routes: Routes = [
     loadChildren: async () => await import('./controllers/faq/faq.module').then((m) => m.FaqModule),
   },
   {
+
     path: 'directory',
     loadChildren: async () =>
       await import('./controllers/directory/directory.module').then((m) => m.DirectoryModule),
   },
-  { path: '**', redirectTo: '/home' },
+  {
+    path: 'achievements',
+    loadChildren: () => import('./controllers/achievements/achievements.module').then(m => m.AchievementsModule)
+  },
+  {path: '**', redirectTo: '/home' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/controllers/home/home.component.ts
+++ src/app/controllers/home/home.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  constructor(private renderer: Renderer2) {
+    renderer.setStyle (
+      document.body,
+      "background-image",
+      "url('/assets/peacuful male warrior.webp')"
+    );
+  }
+}
