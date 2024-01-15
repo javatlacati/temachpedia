deafda32c5928adbb53db003b87905faceca297f
Javatlacati <scherzo_16@hotmail.com>
2024-01-15 05:51:51 -0600

    updated favicon

--- src/app/controllers/search/services/transcript.service.ts
+++ src/app/controllers/search/services/transcript.service.ts
@@ -12,8 +12,8 @@ export class TranscriptService {
   }
 
   public getTransriptForWord(word: string): Observable<Transcription[]> {
-    return this.httpClient.post('http://localhost:8081/api/video-transcription', {
-    // return this.httpClient.post('https://55m31g500e.execute-api.us-east-1.amazonaws.com/default/searchTranscriptionFromPublicElTemachVideos', {
+    // return this.httpClient.post('http://localhost:8081/api/video-transcription', {
+    return this.httpClient.post('https://55m31g500e.execute-api.us-east-1.amazonaws.com/default/searchTranscriptionFromPublicElTemachVideos', {
       "title": word
     }) as Observable<Transcription[]>
   }
